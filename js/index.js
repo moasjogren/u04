3a03d9881dbe853d6b03a91c3104e5643d7589c2
sara-jpallander <sara.j.pallander@gmail.com>
2024-12-06 13:46:08 +0100

    finished styling on home page. Add back to top button and simple page footer

--- js/index.js
+++ js/index.js
@@ -60,7 +60,7 @@ function displayProducts(data) {
    </section>
    <footer class="card-footer">
     <p class="card-price">$${product.price}</p>
-    <button class="card-button" >Add to cart +</button>
+    <button class="card-button" >Add to cart</button>
    </footer>
     </div>
     `;
