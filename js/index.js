71b0e4e3c047e60b887c2067d0665da4f4f216a1
Simon Kane <Simon.kaneborn@chasacademy.se>
2024-12-12 15:21:35 +0100

    leker med search bar

--- js/index.js
+++ js/index.js
@@ -10,12 +10,18 @@ const modalContent = document.createElement("div");
 let cartCounter = 0;
 const cartCount = document.querySelector(".cart-counter");
 const cartLogo = document.querySelector(".cart-logo");
+const searchValue = document.querySelector(".search-input");
+const searchBtn = document.querySelector(".search-btn");
 
-const shoppingCart = localStorage.shoppingCart ? [...JSON.parse(localStorage.shoppingCart)] : [];
+const shoppingCart = localStorage.shoppingCart
+  ? [...JSON.parse(localStorage.shoppingCart)]
+  : [];
 
 let totalShoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
 
-cartCount.innerText = !localStorage.cartCount ? "Empty, Fucking buy something" : localStorage.getItem("cartCount");
+cartCount.innerText = !localStorage.cartCount
+  ? "Empty, Fucking buy something"
+  : localStorage.getItem("cartCount");
 
 filterButton.addEventListener("click", function () {
   document.querySelectorAll("input").forEach((item) => {
@@ -35,11 +41,32 @@ async function getProducts() {
     }
     const data = await response.json();
 
+    searchBtn.addEventListener("click", () => {
+      const searchWord = searchValue.value.toLowerCase();
+
+      if (searchValue.value) {
+        showAll = data.filter((obj) => {
+          obj.title = obj.title.toLowerCase();
+
+          if (obj.title.includes(searchWord)) {
+            obj.title =
+              obj.title.split("")[0].toUpperCase() +
+              obj.title.slice(1, obj.title.length);
+            console.log(showAll);
+            return obj;
+          } else {
+            return;
+          }
+        });
+        displayProducts(showAll);
+      } else {
+        return;
+      }
+      searchValue.value = "";
+    });
+
     showAll = data.filter((obj) => filteredCategorys.includes(obj.category));
 
-    // return item.category.includes(filteredCategorys);
-
-    console.log(showAll);
     if (showAll.length !== 0) {
       displayProducts(showAll);
     } else {
@@ -61,16 +88,16 @@ function displayProducts(data) {
     const card = document.createElement("div");
     card.innerHTML = `
         <div value=${product.id} id="card" class="card">
-   <div class="card-image"> <img class="image" src="${product.image}" alt=""></div>
-   <section class="card-text">
-    <h3 class="card-title" >${limitedTitle}</h3>
-    <p class="card-description" >${limitedText}...</p>
-   </section>
-   <footer class="card-footer">
-    <p class="card-price">$${product.price}</p>
-    <button class="card-button" >Add to cart</button>
-   </footer>
-    </div>
+    <div class="card-image"> <img class="image" src="${product.image}" alt=""></div>
+    <section class="card-text">
+      <h3 class="card-title" >${limitedTitle}</h3>
+      <p class="card-description" >${limitedText}...</p>
+    </section>
+    <footer class="card-footer">
+      <p class="card-price">$${product.price}</p>
+      <button class="card-button" >Add to cart</button>
+    </footer>
+      </div>
   `;
     mainContainer.appendChild(card);
   });
@@ -78,13 +105,10 @@ function displayProducts(data) {
     const cardElement = event.target.closest(".card");
     if (event.target.closest(".card-button")) {
       const chosenCard = data.filter(
-        (product) => product.id === Number(event.target.closest(".card").getAttribute("value"))
+        (product) =>
+          product.id ===
+          Number(event.target.closest(".card").getAttribute("value"))
       );
-
-      // shoppingCart - tom array som ska  fyllas med objekt
-      // För varje 'add to cart' klick - skapa ett objekt med våra olika keys och pusha in i shoppingCart
-      // spara shoppingCart i localStorage
-      // ?
 
       const productInfo = {
         id: "",
@@ -106,35 +130,7 @@ function displayProducts(data) {
         cartLogo.classList.remove("animate");
       }, 800);
 
-      //För att hämta och skriva totalen
-
-      // const shoppingCart = Object.values(localStorage)
-
-      // const total = shoppingCart.reduce((acc, curr) => {
-      //   const prices = JSON.parse(curr)
-      // return acc + prices.price},0)
-      // console.log(total)
-      ///////////////////////////////////////////
-
-      // const totalPrice = JSON.parse(localStorage).reduce((acc, item) => {return acc + item.price},0 )
-
-      // let newString = JSON.stringify(valueArray)
-      // .replace(/[ [ () , "-]/g, " ")
-      // .replace("]", " ");
-      /*   localStorage.setItem(chosenCard[0].title, productInfo);
-      localStorage.setItem(chosenCard[0].price, productInfo); */
-      // data.map((product) => {
-      //   if (product.id == document.querySelector(".card").getAttribute("value")) {
-      //     localStorage.setItem(product.id, productInfo);
-      //   };
-
-      // });
-
-      //cartCounter++
-      //cartCount.innerHTML = cartCounter;
-
       totalShoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
-      console.log(totalShoppingCart.length);
 
       localStorage.setItem("cartCount", totalShoppingCart.length);
 
@@ -164,7 +160,6 @@ function displayProducts(data) {
       } else if (event.target === modal) {
         modal.classList.remove("modal-show");
         modalContent.innerHTML = "";
-        // Kommentar för att göra ny PR :)
       }
     });
   });
