09af6e21a47ebc42e457a191120c0490ef8d965e
sara-jpallander <sara.j.pallander@gmail.com>
2024-12-13 12:16:24 +0100

    styling fixes and added clear cart button

--- js/cart.js
+++ js/cart.js
@@ -3,7 +3,10 @@ const checkoutMessage = document.querySelector(".checkout-message-text");
 const data = localStorage.getItem("shoppingCart");
 const finalCart = document.getElementById("final-cart");
 const cartCount = document.querySelector(".cart-counter");
+const totalPrices = document.getElementById("total-price");
 cartCount.innerText = !localStorage.cartCount ? "Empty, fucking buy something" : localStorage.getItem("cartCount");
+
+const clearCartBtn = document.getElementById('clear-cart-btn');
 
 placeOrder.addEventListener("click", function () {
   checkoutMessage.textContent = "Thank you for your purchase! Please buy more next time :) ";
@@ -11,39 +14,53 @@ placeOrder.addEventListener("click", function () {
 
 function displayShoppingCart() {
   finalCart.innerHTML = "";
-
-  //-------------för att göra nytt object med ammount--------//
   let parsedData = JSON.parse(data);
-  const itemCount = {};
-  parsedData.forEach((item) => {
-    if (itemCount[item.id]) {
-      itemCount[item.id].count++;
-    } else {
-      itemCount[item.id] = { ...item, count: 1 };
-    }
-  });
+  if(parsedData != null) {
+    const itemCount = {};
+    parsedData.forEach((item) => {
+      if (itemCount[item.id]) {
+        itemCount[item.id].count++;
+      } else {
+        itemCount[item.id] = { ...item, count: 1 };
+      }
+    });
+  
+    const shoppingCartarray = Array(...Object.values(itemCount));
+    shoppingCartarray.map((product) => {
+      let displayPrice = ``;
+  
+      if (product.count == 1) {
+        displayPrice = `$${product.price}`;
+      } else if (product.count >= 2) {
+        displayPrice = `$${product.price * product.count} ($${product.price} /each)`;
+      }
+  
+      const item = document.createElement("div");
+      item.innerHTML = `<img src="${product.image}" alt="product-image" height="40px"> <p>X${product.count} ${product.title} - 
+        ${displayPrice} </p>`;
+      finalCart.appendChild(item);
+    });
+    const products = JSON.parse(data);
+    const totatlPrice = products.reduce((acc, product) => acc + product.price, 0);
+    totalPrices.innerHTML = `$${totatlPrice.toFixed(2)}`;
 
-  const shoppingCartarray = Array(...Object.values(itemCount));
-  shoppingCartarray.map((product) => {
-    let displayPrice = ``;
+    clearCartBtn.style.display = "block";
+    
+  } else {
+    clearCartBtn.style.display = "none";
+  }
+  //-------------för att göra nytt object med ammount--------//
 
-    if (product.count == 1) {
-      displayPrice = `$${product.price}`;
-    } else if (product.count >= 2) {
-      displayPrice = `$${product.price * product.count} ($${product.price} /each)`;
-    }
-
-    const item = document.createElement("div");
-    item.innerHTML = `<img src="${product.image}" alt="product-image" height="40px"> <p>X${product.count} ${product.title} - 
-      ${displayPrice} </p>`;
-    finalCart.appendChild(item);
-  });
-  const products = JSON.parse(data);
-  const totatlPrice = products.reduce((acc, product) => acc + product.price, 0);
-  const totalPrices = document.getElementById("total-price");
-  totalPrices.innerHTML = `$${totatlPrice.toFixed(2)}`;
 }
 
 displayShoppingCart();
 
 // Kommentar för ny PR
+
+clearCartBtn.addEventListener('click', () => {
+  localStorage.clear();
+  finalCart.innerHTML = "";
+  totalPrices.innerHTML = "Shopping cart empty."
+  cartCount.innerHTML = "0";
+  clearCartBtn.style.display = "none";
+});
