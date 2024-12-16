f48eaa9e3077fbec500234fe71e5b56abde70cfa
moasjogren <moasjgren@gmail.com>
2024-12-16 13:32:30 +0100

    städad kod

--- js/cart.js
+++ js/cart.js
@@ -4,9 +4,9 @@ const data = localStorage.getItem("shoppingCart");
 const finalCart = document.getElementById("final-cart");
 const cartCount = document.querySelector(".cart-counter");
 const totalPrices = document.getElementById("total-price");
-cartCount.innerText = !localStorage.cartCount ? "Empty, fucking buy something" : localStorage.getItem("cartCount");
 
-const clearCartBtn = document.getElementById('clear-cart-btn');
+cartCount.innerText = !localStorage.cartCount ? "0" : localStorage.getItem("cartCount");
+const clearCartBtn = document.getElementById("clear-cart-btn");
 
 placeOrder.addEventListener("click", function () {
   checkoutMessage.textContent = "Thank you for your purchase! Please buy more next time :) ";
@@ -15,7 +15,7 @@ placeOrder.addEventListener("click", function () {
 function displayShoppingCart() {
   finalCart.innerHTML = "";
   let parsedData = JSON.parse(data);
-  if(parsedData != null) {
+  if (parsedData != null) {
     const itemCount = {};
     parsedData.forEach((item) => {
       if (itemCount[item.id]) {
@@ -24,17 +24,17 @@ function displayShoppingCart() {
         itemCount[item.id] = { ...item, count: 1 };
       }
     });
-  
+
     const shoppingCartarray = Array(...Object.values(itemCount));
     shoppingCartarray.map((product) => {
       let displayPrice = ``;
-  
+
       if (product.count == 1) {
         displayPrice = `$${product.price}`;
       } else if (product.count >= 2) {
         displayPrice = `$${product.price * product.count} ($${product.price} /each)`;
       }
-  
+
       const item = document.createElement("div");
       item.innerHTML = `<img src="${product.image}" alt="product-image" height="40px"> <p>X${product.count} ${product.title} - 
         ${displayPrice} </p>`;
@@ -45,22 +45,17 @@ function displayShoppingCart() {
     totalPrices.innerHTML = `$${totatlPrice.toFixed(2)}`;
 
     clearCartBtn.style.display = "block";
-    
   } else {
     clearCartBtn.style.display = "none";
   }
-  //-------------för att göra nytt object med ammount--------//
-
 }
 
 displayShoppingCart();
 
-// Kommentar för ny PR
-
-clearCartBtn.addEventListener('click', () => {
+clearCartBtn.addEventListener("click", () => {
   localStorage.clear();
   finalCart.innerHTML = "";
-  totalPrices.innerHTML = "Shopping cart empty."
+  totalPrices.innerHTML = "Shopping cart empty.";
   cartCount.innerHTML = "0";
   clearCartBtn.style.display = "none";
 });
